649af6b406164a1fef338671423ecb8d472e415f
Marcos Cesar Gritti <cesargritti@gmail.com>
2018-07-16 18:12:01 -0300

    Removido diretório dist

--- backend/dist/controllers/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const dataset_1 = require("./dataset");
-const material_1 = require("./material");
-exports.default = {
-    Dataset: new dataset_1.DatasetController(),
-    Material: new material_1.MaterialController(),
-};
-//# sourceMappingURL=index.js.map
--- backend/dist/helpers/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const binding_1 = require("./binding");
-exports.autoBind = binding_1.autoBind;
-//# sourceMappingURL=index.js.map
